e9becfa75191de3605ad0ea3c4e2b0edec959c87
Charlie <charlieedoherty@gmail.com>
2024-02-04 16:29:31 -0600

    Finalize my version of a queue

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ from queue import Queue
 if __name__ == "__main__":
     my_queue = Queue()
 
+    print(my_queue.peek_front())
     my_queue.enqueue(1)
+    print(my_queue.peek_front())
+    my_queue.dequeue()
+    print(my_queue.peek_front())
+    my_queue.enqueue(1)
+    print(my_queue.peek_front())
     my_queue.enqueue(2)
-
-    my_queue.peek_front()
+    print(my_queue.peek_front())
+    my_queue.dequeue()
+    print(my_queue.peek_front())
--- queue.py
+++ queue.py
@@ -4,7 +4,7 @@ from node import Node
 class Queue:
 
     def __init__(self):
-        self._top = None
+        self._front = self._rear = None
 
     def enqueue(self, entry):
         """Add to the back"""
@@ -12,25 +12,46 @@ class Queue:
         node = Node(entry)
 
         if self.is_empty():
-            self._top = node
-        else:
-            temp = self._top.next
-            while temp is not None:
-                s
+            self._front = node
+            self._rear = node
 
-    def dequeue(self, node):
+        # Only one item in queue
+        elif self._front == self._rear:
+            self._rear = node
+            self._rear.next = self._front
+
+        # Multiple items in queue
+        else:
+            temp = self._rear
+            self._rear = node
+            self._rear.next = temp
+
+    def dequeue(self):
         """Remove the front"""
 
-        pass
+        # One or no items in the queue
+        if self._front == self._rear:
+            self._front = self._rear = None
+
+        # Multiple items in queue
+        else:
+            # Use a placeholder node to replace the front
+            temp = self._rear
+            while temp.next is not self._front:
+                temp = temp.next
+            self._front = temp
 
     def peek_front(self):
         """Look at the front node"""
 
-        return self._top
+        if self._front:
+            return self._front.value
+        else:
+            return None
 
     def is_empty(self):
         """Checks for any nodes in the queue"""
 
-        if self._top is None:
-            return False
-        return True
+        if self._front is None:
+            return True
+        return False
