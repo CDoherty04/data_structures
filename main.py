cedc3c351a3e65cafc24275252d29776f3f28a60
Charlie <charlieedoherty@gmail.com>
2024-02-04 16:47:00 -0600

    Use main to measure the time efficiency of built structures (not very good)

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-from node import Node
+import time
 from queue import Queue
 
 if __name__ == "__main__":
+
+    start_time = time.time()
+
     my_queue = Queue()
 
-    print(my_queue.peek_front())
-    my_queue.enqueue(1)
-    print(my_queue.peek_front())
-    my_queue.dequeue()
-    print(my_queue.peek_front())
-    my_queue.enqueue(1)
-    print(my_queue.peek_front())
-    my_queue.enqueue(2)
-    print(my_queue.peek_front())
-    my_queue.dequeue()
-    print(my_queue.peek_front())
+    # How many items will be added and removed from the queue
+    iterations = 99999
+
+    for i in range(iterations):
+        my_queue.enqueue(i+1)
+
+    for i in range(iterations):
+        my_queue.dequeue()
+
+    # Calculate time spent running the program
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
+    print(elapsed_time)
